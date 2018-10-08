db7fadddb27051484e8e103988a5fffbc612646b
Son <sonnv.vnu@gmail.com>
2018-10-08 17:56:04 +0700

    page layout (navigation and header) (done) and config (processing)

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -1,23 +1,47 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
 import Header from './Header.js'
 import Navigation from './Navigation.js'
 import PropTypes from 'prop-types'
+import 'antd/dist/antd.less'
 import './PageLayout.scss'
+import { Layout, Icon } from 'antd'
+const { Sider, Content } = Layout
 
-export const PageLayout = ({ children }) => (
-  <div className='page-layout sidebar-mini'>
-    <div id='dashboard-container'>
-      <div className='wrapper'>
-        <Header />
-        <Navigation />
-        <div className='content-wrapper'>
-          {children}
-        </div>
-      </div>
-    </div>
-  </div>
-)
+class PageLayout extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { collapsed: false }
+
+    this.handler = this.handler.bind(this)
+  }
+
+  handler (e) {
+    e.preventDefault()
+    this.setState({
+      collapsed: !this.state.collapsed,
+    })
+  }
+
+  render () {
+    return (
+      <Layout style={{ height: '100%' }}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <Navigation collapsed={this.state.collapsed} />
+        </Sider>
+        <Layout>
+          <Header handler={this.handler} collapsed={this.state.collapsed} />
+          <Content>
+            {this.props.children}
+          </Content>
+        </Layout>
+      </Layout>
+    )
+  }
+}
 
 PageLayout.propTypes = {
   children: PropTypes.node,
